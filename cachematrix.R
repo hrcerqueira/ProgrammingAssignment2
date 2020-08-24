48016c84d72ee0b2f32b533d17fd3fa4e615036a
Hernani <hrcerqueira@gmail.com>
2020-08-24 14:38:31 +0100

    Programming assignemnt exercise. Create two functions around matrix inversion and caching.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions have been developed as part of a Coursera R Programming course
+## assignment. 
 
-## Write a short comment describing this function
+## Creates a set of utility functions to work with a matrix and cache the results of
+## inverting it. It's assumed the matrix is  always invertible.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  
+  get <- function() x
+  
+  setsolve <- function(solve) s <<- solve
+  
+  getsolve <- function() s
+  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Inverts a matrix, but caches the result for subsequent invocations. 
+## x should be a list of functions as returned by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  
+  s
 }
